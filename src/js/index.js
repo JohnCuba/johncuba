10a44541231f9a65f0878fd91775364e5b128053
Evgeny Kubecky <johncuba@yandex.ru>
2021-09-05 20:25:46 +0300

    feat: init color theme

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import './lib/fallback-color-scheme'
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { resolve } from 'path'
 
 export default {
   root: 'src',
+  publicDir: '../public',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
+    cssCodeSplit: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
