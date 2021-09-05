72589024ed9cc3a2ef8aeee775ab892adb878a82
Evgeny Kubecky <johncuba@yandex.ru>
2021-09-05 19:05:34 +0300

    fix: change outDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 export default {
   root: 'src',
   build: {
+    outDir: '../dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
