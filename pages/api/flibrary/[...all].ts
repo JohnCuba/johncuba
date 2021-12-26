70c4b2a6488e612e02090a5388b52a990ed389c1
Evgeny Kubecky <johncuba@yandex.ru>
2021-12-26 14:03:23 +0300

    feat: add flibrary proxy

---
+++ pages/api/flibrary/[...all].ts
@@ -0,0 +1,26 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import httpProxyMiddleware from 'next-http-proxy-middleware';
+
+const HOST = process.env.FLIBRARY_API_HOST || '';
+const ENTRYPOINT = process.env.FLIBRARY_API_ENTRYPOINT || '';
+
+const KEY = process.env.FLIBRARY_API_KEY || 'key';
+const VALUE = process.env.FLIBRARY_API_VALUE || 'value';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.body[KEY] === VALUE) {
+    httpProxyMiddleware(req, res, {
+      target: HOST,
+      pathRewrite: [{
+        patternStr: '^/api/flibrary',
+        replaceStr: ENTRYPOINT,
+      }]
+    })
+  } else {
+    res.statusCode = 401;
+    res.statusMessage = 'Please provide valid credentials';
+    res.end();
+  }
+}
+
+export default handler;
