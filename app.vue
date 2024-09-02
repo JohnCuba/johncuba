8f85aeeb34ee798140def495eb538200609963ed
JohnCuba <ekubetsky@gmail.com>
2024-09-02 12:25:12 +0300

    feat: add analytic

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    (import.meta.dev ? {} : {
+      src: 'https://umami.johncuba.space/script.js',
+      defer: true,
+      'data-website-id': '463d4ef6-616d-49b1-9ebe-c98f488b5ec4',
+    })
+  ]
+})
+</script>
+
 <template>
   <NuxtPage />
 </template>
