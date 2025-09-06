f37729bfd10cd3647707ffe636e0fc1689d7df39
JohnCuba <ekubetsky@gmail.com>
2025-09-06 12:47:58 +0300

    fix: text fill

--- components/pong/lib/shared/styled-text.ts
+++ components/pong/lib/shared/styled-text.ts
@@ -12,10 +12,15 @@ export class StyledText extends Text {
 			style: new TextStyle({
 				fontFamily: 'New Amsterdam',
 				fontSize: 64,
-				fill: new FillGradient(0, 0, 0, 0).addColorStop(
-					0,
-					Color.shared.setValue(0xffffff),
-				),
+				fill: new FillGradient({
+					type: 'linear',
+					colorStops: [
+						{
+							offset: 0,
+							color: Color.shared.setValue(0xffffff),
+						},
+					],
+				}),
 				...options.style,
 			}),
 			...options,
